4f2b68fb296b487185bf011cc9ad955f1e31432d
carlosmelendrez31 <carlosmelendrez31@outlook.com>
2025-01-29 15:38:46 -0600

    interfaces

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Trashtec
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Trashtec
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
